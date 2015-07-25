fc64cfe29f0e185decb56d5762766711a7f52925
Cyrus Xi <xi.cyrus@gmail.com>
2015-07-25 06:47:15 -0400

    Clean up parsing code

--- parse_transcript.py
+++ parse_transcript.py
@@ -23,24 +23,26 @@ message_blocks = soup.find_all('div', class_ = 'messageBlock')
 if not shelf_dict: SHELF_EXISTED = False
 else: SHELF_EXISTED = True
 
+# If key not in dict, don't throw KeyError and initialize with an empty list value.
 master_dict = defaultdict(list)
 
 for block in message_blocks:
     # Ignore system messages.
-    if block['timestamp'] == '': continue
+    if not block['timestamp']: continue
     # Unix-style timestamp.
     timestamp = int(block['timestamp'])
     # Get all the titles from the other* divs, including otherCont*.
     title = block.find('div', class_ = compile('other'))['title']
-    # Remove time from title (e.g., title="Xi, Cyrus [9:42:54 AM]").
+    # Get and remove time from title (e.g., title="Xi, Cyrus [9:42:54 AM]").
     time_pattern = compile(r'\[.+\]')
     time = time_pattern.search(title).group()[1:-1]
+    # The name is what's left.
     name = time_pattern.sub('', title)[:-1]
     # Get the text from all message* (including messageCont*) divs.
     message = block.find('div', class_ = compile('message')).get_text()[:-1]
     message = message.encode('utf-8')
+    # Store both time and timestamp, because timestamps are unique and can order.
     master_val_tup = (message, time, timestamp)
-    # Because defaultdict, if name not a key, will create it with empty list value.
     master_dict[name].append(master_val_tup)
 
 master_dict = dict(master_dict)
@@ -50,11 +52,11 @@ if SHELF_EXISTED:
     keys = set(shelf_dict['my_dict']).union(master_dict)
     no = []
     # Generator expression.
+    # Make use of d.get(k, default) functionality.
     master_dict = dict((k, shelf_dict['my_dict'].get(k, no) + master_dict.get(k, no)) for k in keys)
         
 # Pretty print.
 pprint(master_dict)
-print "\nNames: " + str(master_dict.keys())
 
 # Update shelf.
 shelf_dict['my_dict'] = master_dict
