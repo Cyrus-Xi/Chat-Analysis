b7088a81688b3310c644ff0f4aad6408151dae24
Cyrus Xi <xi.cyrus@gmail.com>
2015-07-25 08:22:59 -0400

    Simple wordcloud creation for user chat logs

---
+++ create_wordcloud.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from sys import argv
+
+try:
+    filename = argv[1]
+except:
+    print "Please provide a filename argument. Exiting."
+    exit(1)
+
+text = open(filename).read()
+wordcloud = WordCloud().generate(text)
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
